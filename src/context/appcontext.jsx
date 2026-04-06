c0bcb63ca356a3c3443caa50933e8a0d91f1adf9
shaikdavoodbasha <shannushaik080@gmail.com>
2026-04-06 11:31:59 +0530

    added go

--- src/context/appcontext.jsx
+++ src/context/appcontext.jsx
@@ -1,15 +1,15 @@
-// import { createContext } from "react";
+import { createContext } from "react";
 
-// export const MyAppContext = createContext();
+export const MyAppContext = createContext();
 
-// export const RealContext = (props) => {
-//   const value = {
-//     name1: "Salar",
-//     age: "34",
-//   };
-//   return (
-//     <MyAppContext.Provider value={value}>
-//       {props.children}
-//     </MyAppContext.Provider>
-//   );
-// };
+export const RealContext = (props) => {
+  const value = {
+    name1: "Salar",
+    age: "34",
+  };
+  return (
+    <MyAppContext.Provider value={value}>
+      {props.children}
+    </MyAppContext.Provider>
+  );
+};
