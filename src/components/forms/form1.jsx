40a8197cb8eea789b89a39286bdc7202ede69ce8
shaikdavoodbasha <shannushaik080@gmail.com>
2026-04-12 02:23:40 +0530

    view one

--- src/components/forms/form1.jsx
+++ src/components/forms/form1.jsx
@@ -12,12 +12,13 @@ const Form1 = ({children}) => {
 
     // }
 
+    
     const[formData,setFormData] = useState({
         fname:"",
         femail:"",
         fpassword:""
     })
-    
+
     const [fullData,setFullData]=useState([])
     const handleform = (e)=>{
         const {name,value} = e.target;
