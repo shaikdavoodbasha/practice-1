c403f547baf47d0c0fb8a2af22dd5de11af8d518
shaikdavoodbasha <shannushaik080@gmail.com>
2026-04-13 12:48:37 +0530

    view3

--- src/components/one1.jsx
+++ src/components/one1.jsx
@@ -1,43 +1,43 @@
-// import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 
-// const One1 = () => {
-//   const [data, setData] = useState([]);
+const One1 = () => {
+  const [data, setData] = useState([]);
 
-//   const fetchApiData = async () => {
-//     const api = await fetch("https://jsonplaceholder.typicode.com/posts");
-//     const apiData = await api.json();
-//     setData(apiData);
-//   };
+  const fetchApiData = async () => {
+    const api = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const apiData = await api.json();
+    setData(apiData);
+  };
 
-//   useEffect(() => {
-//     fetchApiData();
-//     // fetch("https://jsonplaceholder.typicode.com/posts")
-//     //   .then((res) => res.json())
-//     //   .then((res) => setData(res))
-//     //   .catch((err) => console.log(err));
+  useEffect(() => {
+    fetchApiData();
+    // fetch("https://jsonplaceholder.typicode.com/posts")
+    //   .then((res) => res.json())
+    //   .then((res) => setData(res))
+    //   .catch((err) => console.log(err));
 
-//     // console.log(data)
-//   }, []);
+    // console.log(data)
+  }, []);
 
-//   useEffect(() => {
-//     console.log(data);
-//   }, [data]);
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
 
-//   return (
-//     <div>
-//       <h1 className="text-center text-2xl text-red-500 font-bold">
-//         This is for checking api
-//       </h1>
-//       {data.map((item, index) => {
-//         return (
-//           <div key={index}>
-//             <h1 className="text-blue-800">{item.body}</h1>
-//             <h1 className="text-yellow-300">{item.title}</h1>
-//           </div>
-//         );
-//       })}
-//     </div>
-//   );
-// };
+  return (
+    <div>
+      <h1 className="text-center text-2xl text-red-500 font-bold">
+        This is for checking api
+      </h1>
+      {data.map((item, index) => {
+        return (
+          <div key={index}>
+            <h1 className="text-blue-800">{item.body}</h1>
+            <h1 className="text-yellow-300">{item.title}</h1>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-// export default One1;
+export default One1;
