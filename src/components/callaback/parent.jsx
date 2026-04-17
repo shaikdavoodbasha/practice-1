7b7ded96d2c78b917ca406c56bad3ef6681f9e5e
shaikdavoodbasha <shannushaik080@gmail.com>
2026-04-17 09:33:39 +0530

    form-2

--- src/components/callaback/parent.jsx
+++ src/components/callaback/parent.jsx
@@ -1,21 +1,21 @@
-// import React, { useContext, useState } from 'react'
-// import Child from './child'
-// import { MainContext } from '../../context/appcontext'
+import React, { useContext, useState } from 'react'
+import Child from './child'
+import { MainContext } from '../../context/appcontext'
 
-// const Parent = () => {
-//     const {name1} = useContext(MainContext)
-//     const [childData,setChildData] = useState('')
-//     const fromChild = (childData)=>{
-//         setChildData(childData)
+const Parent = () => {
+    const {name1} = useContext(MainContext)
+    const [childData,setChildData] = useState('')
+    const fromChild = (childData)=>{
+        setChildData(childData)
 
-//     }
-//   return (
-//     <div>
-//         <h1>This is from context api {name1}</h1>
-//       <Child sendDataToParent={fromChild} />
-//       <h1>This is from child {childData}</h1>
-//     </div>
-//   )
-// }
+    }
+  return (
+    <div>
+        <h1>This is from context api {name1}</h1>
+      <Child sendDataToParent={fromChild} />
+      <h1>This is from child {childData}</h1>
+    </div>
+  )
+}
 
-// export default Parent
+export default Parent
