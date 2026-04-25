401693aec50976f5eddd26f06939e5c0140c44bd
shaikdavoodbasha <shannushaik080@gmail.com>
2026-04-25 10:29:25 +0530

    chidl-2

--- src/components/callaback/child.jsx
+++ src/components/callaback/child.jsx
@@ -1,16 +1,16 @@
-import React, { useState } from 'react'
+// import React, { useState } from 'react'
 
-const Child = ({sendDataToParent}) => {
-    const[input,setInput]  =useState("")
-    const handleInput = ()=>{
-        sendDataToParent(input)
-    }
-  return (
-    <div>
-        <input type="text" value={input} onChange={(e)=>setInput(e.target.value)}/>
-        <button onClick={handleInput}>Sending Data to parent</button>
-    </div>
-  )
-}
+// const Child = ({sendDataToParent}) => {
+//     const[input,setInput]  =useState("")
+//     const handleInput = ()=>{
+//         sendDataToParent(input)
+//     }
+//   return (
+//     <div>
+//         <input type="text" value={input} onChange={(e)=>setInput(e.target.value)}/>
+//         <button onClick={handleInput}>Sending Data to parent</button>
+//     </div>
+//   )
+// }
 
-export default Child
+// export default Child
