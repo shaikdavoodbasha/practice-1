143b82cbb3b78c7e1459bb904bf1d4961a721e9f
shaikdavoodbasha <shannushaik080@gmail.com>
2026-09-13 12:37:27 +0530

    tree-threeseven

--- src/components/one1.jsx
+++ src/components/one1.jsx
@@ -31,7 +31,7 @@ const One1 = () => {
       {data.map((item, index) => {
         return (
           <div key={index}>
-            <h1 className="text-blue-800">{item.body}</h1>
+            {/* <h1 className="text-blue-800">{item.body}</h1> */}
             <h1 className="text-yellow-300">{item.title}</h1>
           </div>
         );
